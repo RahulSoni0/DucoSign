50afedfcacfc2456cfadab65b2e33060a91937c9
Joaquin144 <vvpp898.comco@gmail.com>
2022-08-06 04:47:59 +0530

    listeners added

---
+++ app/src/main/java/com/devcommop/joaquin/codeforgood/common/listener/OnClickStudent.kt
@@ -0,0 +1,21 @@
+package com.devcommop.joaquin.codeforgood.common.listener
+
+interface OnClickStudent {
+    fun onClick()
+}
+interface OnClickClass{
+    fun onClick()
+}
+
+interface OnClickStudentAdd {
+    fun onClick()
+    fun onClickAdd()
+}
+
+interface OnClickStudentAddRemove {
+    fun onClick()
+    fun onClickAdd()
+    fun onClickSubtract()
+    fun onClickRemove()
+
+}
---
+++ app/src/main/java/com/devcommop/joaquin/codeforgood/common/listener/OnSearchListener.kt
@@ -0,0 +1,5 @@
+package com.devcommop.joaquin.codeforgood.common.listener
+
+interface OnSearchListener {
+    fun onSearch(user: String)//todo: Make it UserEntity or more flexible search
+}
