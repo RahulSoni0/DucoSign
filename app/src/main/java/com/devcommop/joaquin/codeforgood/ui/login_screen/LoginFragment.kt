a67d546d7567cb43831c382d3034b3608d4cb5bb
Subhadiptech <subhadipdhn@gmail.com>
2022-08-07 16:57:19 +0530

    login structure done

--- app/src/main/java/com/devcommop/joaquin/codeforgood/ui/login_screen/LoginFragment.kt
+++ app/src/main/java/com/devcommop/joaquin/codeforgood/ui/login_screen/LoginFragment.kt
@@ -1,12 +1,22 @@
 package com.devcommop.joaquin.codeforgood.ui.login_screen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.devcommop.joaquin.codeforgood.R
+import com.devcommop.joaquin.codeforgood.databinding.FragmentLoginBinding
 
 class LoginFragment : Fragment(R.layout.fragment_login) {
 
+    private lateinit var binding: FragmentLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentLoginBinding.inflate(layoutInflater)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
 }
--- app/src/main/java/com/devcommop/joaquin/codeforgood/ui/signup_screen/SignUpViewModel.kt
+++
@@ -1,7 +0,0 @@
-package com.devcommop.joaquin.codeforgood.ui.signup_screen
-
-import androidx.lifecycle.ViewModel
-import com.devcommop.joaquin.codeforgood.domain.repository.Repository
-
-class SignUpViewModel(val repository: Repository): ViewModel() {
-}
