2276b32647bc7b0d7d0d09f8d0e3ee1d76c995d4
rahulsoni0 <rahul.kumar.soni2511@gmail.com>
2022-08-09 16:24:53 +0530

    added btm nav

--- app/src/main/java/com/devcommop/joaquin/codeforgood/ui/MainActivity.kt
+++ app/src/main/java/com/devcommop/joaquin/codeforgood/ui/MainActivity.kt
@@ -2,11 +2,34 @@ package com.devcommop.joaquin.codeforgood.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.devcommop.joaquin.codeforgood.R
+import com.devcommop.joaquin.codeforgood.ui.home_screen.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //binding.bottomBar.setOnTabSelectListener(object : AnimatedBottomBar.OnTabSelectListener {
+        //             override fun onTabSelected(
+        //                 lastIndex: Int,
+        //                 lastTab: AnimatedBottomBar.Tab?,
+        //                 newIndex: Int,
+        //                 newTab: AnimatedBottomBar.Tab
+        //             ) {
+        //                 Toast.makeText(context, "Selected index: $newIndex, title: ${newTab.title}",Toast.LENGTH_SHORT).show()
+        //             }
+        //
+        //             // An optional method that will be fired whenever an already selected tab has been selected again.
+        //             override fun onTabReselected(index: Int, tab: AnimatedBottomBar.Tab) {
+        //                 Log.d("bottom_bar", "Reselected index: $index, title: ${tab.title}")
+        //             }
+        //         })
+
+
     }
-}
+
+
+
+    }
--- app/src/main/java/com/devcommop/joaquin/codeforgood/ui/home_screen/HomeFragment.kt
+++ app/src/main/java/com/devcommop/joaquin/codeforgood/ui/home_screen/HomeFragment.kt
@@ -1,16 +1,29 @@
 package com.devcommop.joaquin.codeforgood.ui.home_screen
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.devcommop.joaquin.codeforgood.R
+import com.devcommop.joaquin.codeforgood.databinding.ActivityMainBinding
+import com.devcommop.joaquin.codeforgood.databinding.FragmentHomeBinding
+import nl.joery.animatedbottombar.AnimatedBottomBar
 
 class HomeFragment : Fragment(R.layout.fragment_home) {
+    private lateinit var binding: FragmentHomeBinding
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = FragmentHomeBinding.inflate(layoutInflater)
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+
     }
 
 }
